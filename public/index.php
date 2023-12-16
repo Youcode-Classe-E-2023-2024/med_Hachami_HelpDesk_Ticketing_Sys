0505fa1d141d161b920b94870624ea6e9bf8cd24
MedHachami <moha.hach.9@gmail.com>
2023-12-16 01:30:51 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
   require_once '../app/bootstrap.php';
 
   // Init Core Library
