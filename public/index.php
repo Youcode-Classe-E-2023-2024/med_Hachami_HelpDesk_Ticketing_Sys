7106f269a12c4844ac31831a6cda25b2017272a7
MedHachami <moha.hach.9@gmail.com>
2023-12-17 19:08:51 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 
-header("Access-Control-Allow-Origin: *"); 
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+  header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+  // Respond with a 200 OK status
+  http_response_code(200);
+  exit;
+}
 
   require_once '../app/bootstrap.php';
 
