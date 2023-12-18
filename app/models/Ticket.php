373a2caabdfaf72c7b8b42cce4d955da00d93833
MedHachami <moha.hach.9@gmail.com>
2023-12-18 23:59:56 +0100

    Update Ticket.php

--- app/models/Ticket.php
+++ app/models/Ticket.php
@@ -8,12 +8,12 @@ Class Ticket{
     }
 
     public function addTicket($data){
-        $this->db->query("INSERT INTO ticket (title , description , priority , tag , creatordId) 
-                            VALUES (:title, :description, :priority , :tag , :creatordId)"); 
+        $lastTicketId = '';
+        $this->db->query("INSERT INTO ticket (title , description , priority , creatordId) 
+                            VALUES (:title, :description, :priority ,:creatordId)"); 
         $this->db->bind(':title',$data['title']);
         $this->db->bind(':description',$data['description']);
         $this->db->bind(':priority',$data['priority']);
-        $this->db->bind(':tag',$data['tag']);
         $this->db->bind(':creatordId',$data['creatordId']);
         $insertIntoTicket = $this->db->execute();
         if($insertIntoTicket){
@@ -26,10 +26,106 @@ Class Ticket{
                 $assigning = $this->db->execute();
 
             }  
-            return $assigning ;   
+            foreach ($data['tags'] as $tagId) {
+                $this->db->query("INSERT INTO ticket_tag(ticket_id, tag_id) VALUES (:ticket_id, :tag_id) ");
+                $this->db->bind(':ticket_id',$lastTicketId);
+                $this->db->bind(':tag_id',$tagId);
+                $ticketTag = $this->db->execute();
+            }
+            return $assigning && $ticketTag ;   
         }else{
             return false ;
         }
         
+    }
+
+    public function getAllTickets(){
+        $this->db->query("SELECT
+        DISTINCT ticket.id as tickeId,
+        ticket.title as ticketTitle,
+        ticket.description as ticketDesc,
+        ticket.createdAt as ticketCreatedAt,
+        user.id as creatordId,
+        user.full_name as creatorName,
+        user.imgUrl as creatorImg,
+        priority.Name as priority,
+        status.Name as status,
+        GROUP_CONCAT(DISTINCT tags.Name) as tags,
+        (
+            SELECT GROUP_CONCAT( assignedUser.imgUrl)
+            FROM ticketassignment
+            LEFT JOIN user as assignedUser ON ticketassignment.userId = assignedUser.id
+            WHERE ticketassignment.ticketId = ticket.id
+        ) as assignedUserImg
+    FROM
+        ticket
+    JOIN priority ON ticket.priority = priority.id
+    JOIN status ON ticket.status = status.id
+    INNER JOIN ticketassignment ON ticket.id = ticketassignment.ticketId
+    INNER JOIN user ON ticket.creatordId = user.id
+    INNER JOIN ticket_tag ON ticket.id = ticket_tag.ticket_id
+    INNER JOIN tags ON ticket_tag.tag_id = tags.id
+    GROUP BY
+        tickeId,
+        ticketTitle,
+        ticketDesc,
+        ticketCreatedAt,
+        userId,
+        creatorName,
+        priority,
+        status;
+    ");
+        return $this->db->resultSet();
+        
+    }
+
+    public function getTicketAssignedTo($userId){
+        $this->db->query("
+        SELECT
+        DISTINCT ticket.id as tickeId,
+        ticket.title as ticketTitle,
+        ticket.description as ticketDesc,
+        ticket.createdAt as ticketCreatedAt,
+        user.id as creatordId,
+        user.full_name as creatorName,
+        user.imgUrl as creatorImg,
+        priority.Name as priority,
+        status.Name as status,
+        GROUP_CONCAT(DISTINCT tags.Name) as tags,
+        (
+            SELECT GROUP_CONCAT( assignedUser.imgUrl)
+            FROM ticketassignment
+            LEFT JOIN user as assignedUser ON ticketassignment.userId = assignedUser.id
+            WHERE ticketassignment.ticketId = ticket.id 
+        ) as assignedUserImg
+    FROM
+        ticket
+    JOIN priority ON ticket.priority = priority.id
+    JOIN status ON ticket.status = status.id
+    INNER JOIN ticketassignment ON ticket.id = ticketassignment.ticketId
+    INNER JOIN user ON ticket.creatordId = user.id
+    INNER JOIN ticket_tag ON ticket.id = ticket_tag.ticket_id
+    INNER JOIN tags ON ticket_tag.tag_id = tags.id
+    AND ticketassignment.userId=:userId 
+    GROUP BY
+        tickeId,
+        ticketTitle,
+        ticketDesc,
+        ticketCreatedAt,
+        userId,
+        creatorName,
+        priority,
+        status;
+        
+        ");
+        $this->db->bind(':userId',$userId);
+        return $this->db->resultSet();
+    }
+
+    public function assignedTo($ticketId){
+        $this->db->query("SELECT user.id , user.full_name, user.email  , ticket.id as tickeId FROM ticketassignment , ticket, user 
+        WHERE ticket.id = ticketassignment.ticketId AND ticketassignment.userId = user.id AND ticketassignment.ticketId = :tickedId");
+        $this->db->bind(':tickedId' , $ticketId);
+        return $this->db->resultSet();
     }
 }
